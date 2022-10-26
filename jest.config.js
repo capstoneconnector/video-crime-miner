4ccedc8e63325cfe0f52ff6934888fcd4455f7dd
Merghani Hag Elnour <Mthagelnour@bsu.edu>
2022-10-26 02:49:16 -0400

    update package.json dependencies Add comment to explain what jest.config.js does

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
+
+/**
+ * The jest.config.js file is used for configuring Jest, 
+ * The modulePaths property tells Jest where it can find modules used inside of the code you're testing.
+ */
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
