7c7bbc958dfd585a3ecb3975d1cd160b3dbeac1c
Jacob Bishop <jacobbishop1000@gmail.com>
2022-11-05 13:39:50 -0400

    Progress submitted for Isaac to build on

--- src/video/videoUtils.ts
+++ src/video/videoUtils.ts
@@ -1,14 +1,44 @@
 require("dotenv").config()
-const rekognition = require("@aws-sdk/client-rekognition");
+const AWS = require("@aws-sdk/client-rekognition");
 
-function getVideoOutput(){
-    
-    return "{}"
+// const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+var jobId = startVideoFacesDetection()
+var output = getVideoFacesDetectionOutput(jobId)
+
+function connect() {
+	const rekognition = new AWS({
+		accessKeyId,
+		secretAccessKey
+	})
+
+	return rekognition
 }
 
+function startVideoFacesDetection(){
+    var rekognition = connect()
+    var attributes = {
+        "FaceAttributes": "DEFAULT",
+        "JobTag": "string",
+        "Video": { 
+           "S3Object": { 
+              "Bucket": "test-videos-video-crime-processor",
+              "Name": "Garland police releases footage of gunman in fatal shooting of 3 teens at gas station-djU2QN9CI_Y.mp4",
+           }
+        }
+     }
+    return rekognition.StartFaceDetection(attributes)
+}
+
+function getVideoFacesDetectionOutput(jobId:string){
+    var rekognition = connect()
+    return rekognition.getFaceDetection()
+}
 
 function sendRequest(){
-    
+    var rekognition = connect()
     rekognition.compareFaces({}, function (err: { stack: any; }, data: any) {
         if (err) console.log(err, err.stack); // an error occurred
         else     console.log(data);           // successful response
@@ -16,3 +46,5 @@ function sendRequest(){
     
     return "https://aws.com/"
 }
+
+export {startVideoFacesDetection, getVideoFacesDetectionOutput}
