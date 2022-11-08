59a11d4af9c7e996119cddac227cdbec2cfbabbc
Jacob Bishop <jacobbishop1000@gmail.com>
2022-11-08 12:19:17 -0500

    Added start to upload file frontend integration

--- src/node/rekognitionConnector.ts
+++ src/node/rekognitionConnector.ts
@@ -1,5 +1,11 @@
 // Angular-Video Connector Module
 
-function UploadVideoOrImage(){
-    
+function UploadVideoOrImage(form:any){
+    // get file path
+    try{
+        console.log(form)
+    } catch (error){
+        console.log("error")
+    }
+    // call upload in S3Connector with that file path
 }
