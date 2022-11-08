4e7e46f1e7e1fdd61d04c9d7e469c60407e20707
Jacob Bishop <jacobbishop1000@gmail.com>
2022-11-08 14:31:01 -0500

    cli kind of updated?

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
+const { fail } = require("assert")
+
 function main(){
+
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+      })
+
     while (true){
-        var choices = "(1) Upload a file\n(2)Scan for faces in a video\n(3) Label Detection for Video\n(4)View my files\n(q) Quit"
+        var choices = "(1) Upload a file\n(2) Scan for faces in a video\n(3) Label Detection for Video\n(4) View my files\n(q) Quit"
         var input = ""
         console.log("What would you like to do?")
         console.log(choices)
-        input = prompt()
+        readline.question("Choose an option: ", x => {
+        input = x
         if(input=="q"){
+            //fail()
             //exit the program
         }else if(input=="1"){
             //upload a file
@@ -18,6 +28,9 @@ function main(){
         }else{
             console.log("ERROR: invalid choice")
         }
+    })
 
     }
 }
+
+main()
