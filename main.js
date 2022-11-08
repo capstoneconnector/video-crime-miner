3376dcaffeb74d73c934cc175c0e12ee56121dfb
Jacob Bishop <jacobbishop1000@gmail.com>
2022-11-08 15:08:35 -0500

    Upload file works in CLI

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const { fail } = require("assert")
-const { resolve } = require("path")
+const { upload } = require("./src/video/s3Connector.js")
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
 
 function main(){
-
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-      })
-
     var choices = "(1) Upload a file\n(2) Scan for faces in a video\n(3) Label Detection for Video\n(4) View my files\n(q) Quit"
-    var input = ""
     console.log("What would you like to do?")
     console.log(choices)
+
     readline.question("Choose an option: ", x => {
-    resolveInput(x)
+        resolveInput(x)
     })
 }
 
@@ -24,7 +23,11 @@ function resolveInput(userInput){
         fail("user quit the program")
     }else if(userInput=="1"){
         //upload a file
-        main()
+        readline.question("Input file path: ", x => {
+            upload("video-crime-miner-video-test-bucket", x).then(response => {
+                console.log(response)
+            })
+        })
     }else if(userInput=="2"){
         //scan video faces
         main()
