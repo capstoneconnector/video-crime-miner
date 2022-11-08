9150ff1731b8e685dd2d1b07fb226e4a8010208e
Jacob Bishop <jacobbishop1000@gmail.com>
2022-11-08 12:58:25 -0500

    Halfway done with calling function to frontend

--- src/node/rekognitionConnector.ts
+++ src/node/rekognitionConnector.ts
@@ -1,11 +1,15 @@
 // Angular-Video Connector Module
+import { upload } from "../video/s3Connector.js"
 
-function UploadVideoOrImage(form:any){
+export default ()= function UploadVideoOrImage(form:any){
     // get file path
     try{
         console.log(form)
+        upload("video-crime-miner-video-test-bucket", form).then(response => {
+            console.log(response)
+        })
     } catch (error){
-        console.log("error")
+        console.log(error)
     }
     // call upload in S3Connector with that file path
 }
