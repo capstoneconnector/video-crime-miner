db3c73d8421ca2d1de11f75a3a3fec18cdf15b4f
ejperezz <ejperez@bsu.edu>
2022-11-08 21:42:29 -0500

    updated and fixed body.component files

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  styleUrls: ['./body.component.scss']
 })
 export class BodyComponent implements OnInit {
 
