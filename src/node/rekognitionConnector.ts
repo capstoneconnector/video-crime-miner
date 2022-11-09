eebade02eeb2330b7cf6bd4b1a29d703bd796306
ejperezz <ejperez@bsu.edu>
2022-11-08 22:25:27 -0500

    updated and fixed

--- src/node/rekognitionConnector.ts
+++ src/node/rekognitionConnector.ts
@@ -3,16 +3,16 @@
 //const upload = require("../video/s3Connector.js")
 //import { upload } from "../video/s3Connector.js"
 //var exports = {}
-import { upload } from '../video/s3Connector.js'
+//import { upload } from '../video/s3Connector.js'
 //import * as something from "../video/s3Connector.js"
 
 function UploadVideoOrImage(form:any){
     // get file path
     try{
         console.log(form)
-        upload("video-crime-miner-video-test-bucket", form).then((response: any) => {
-            console.log(response)
-        })
+        //upload("video-crime-miner-video-test-bucket", form).then((response: any) => {
+            //console.log(response)
+        //})
     } catch (error){
         console.log(error)
     }
