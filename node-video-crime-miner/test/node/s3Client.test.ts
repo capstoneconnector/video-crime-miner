e724066fc49ed6fb9032f28707281fa57d43272f
Merghani Hag Elnour <Mthagelnour@bsu.edu>
2022-11-09 20:23:27 -0500

    Mock S3 client

--- node-video-crime-miner/test/node/s3Client.test.ts
+++ node-video-crime-miner/test/node/s3Client.test.ts
@@ -1,29 +1,29 @@
-import { connect, createBucket, listBuckets, listObjects, upload } from "../../src/node/s3Connector"
+var s3 = require("../../src/node/s3Connector.ts")
+
+jest.mock(s3)
 
 describe("connect function", () => {
 
 	test("Should return a response from aws after sending required params", () => {
-		const res = connect()
+		const res = s3.connect()
 		expect(res).toBeTruthy()
 	})
 })
 
+
 describe("createBucket function", () => {
 
 	test("Should return a response after sending a request to create bucket", () => {
-		const res = createBucket("mt-vcm-uploads")
+		const res = s3.createBucket("mt-vcm-uploads")
 		console.log(res)
 		expect(res).toBeTruthy()
 	})
 })
 
-
-
-
 describe('listObjects function', () => {
 
 	it('Should return a response in dictionary format', async() => {
-		const res = await listObjects("mt-vcm-uploads")
+		const res = await s3.listObjects("mt-vcm-uploads")
 		console.log(res)
 		expect(res).toBeTruthy()
 	})
@@ -34,23 +34,22 @@ describe('listObjects function', () => {
 describe("listBucket function", () => {
 
 	it("Should return a list of buckets", async() => {
-		const res = await listBuckets()
+		const res = await s3.listBuckets()
 		console.log(res)
 		expect(res).toBeTruthy()
 	})
 })
 
-
 describe("upload function", () => {
 
 	it("Should return successfull response after uploading image file", async () => {
-		const res = await upload("mt-vcm-uploads", "resources/beach.jpg")
+		const res = await s3.upload("mt-vcm-uploads", "resources/beach.jpg")
 		console.log(res)
 		expect(res).toBeTruthy()
 	})
 
 	it("Should return successfull response after uploading video file", async () => {
-		const res = await upload("mt-vcm-uploads", "resources/testVideo.mp4")
+		const res = await s3.upload("mt-vcm-uploads", "resources/testVideo.mp4")
 		console.log(res)
 		expect(res).toBeTruthy()
 	})
