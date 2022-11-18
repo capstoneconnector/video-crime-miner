b591eea7cdf240169f3c2a1817923e2caf9d9e22
Illuminubby <illuminubby@gmail.com>
2022-11-18 13:01:18 -0500

    add test for subscribeSQStoSNS function in src/AWSLayer/snsClient.ts

--- node-video-crime-miner/test/AWS Layer/snsClient.test.ts
+++ node-video-crime-miner/test/AWS Layer/snsClient.test.ts
@@ -1,6 +1,6 @@
 import { mockClient } from "aws-sdk-client-mock"
-import { SNSClient, CreateTopicCommand } from "@aws-sdk/client-sns"
-import { createTopic } from "../../src/AWS Layer/snsClient"
+import { SNSClient, CreateTopicCommand, SubscribeCommand } from "@aws-sdk/client-sns"
+import { createTopic, subscribeSQStoSNS } from "../../src/AWS Layer/snsClient"
 
 var snsMock = mockClient(SNSClient)
 
@@ -17,3 +17,18 @@ describe("createTopic function", () => {
 		expect(res).toBeTruthy()
 	})
 })
+
+describe("subscribeSQStoSNS function", () => {
+
+	test("Should return a response from aws after sending required params", async() => {
+		
+        snsMock.on(SubscribeCommand).resolves({
+            //insert example json stub here
+            "SubscriptionArn": "alkjsdhfjkh4jkhfjkldj3"
+        })
+
+        const res = await subscribeSQStoSNS("exampleARN", "exampleQueueARN",'sqs', snsMock)
+		expect(res).toBeTruthy()
+	})
+})
+
