1a03efa823051e4e18461e6bbf6b98adc3c248c9
Illuminubby <illuminubby@gmail.com>
2022-11-18 13:48:55 -0500

    expanded parameters pf createQueue & getQueueUrl functions to take SQSClient object to send commands, mainly for testing with mock clients

--- node-video-crime-miner/src/AWS Layer/sqsClient.ts
+++ node-video-crime-miner/src/AWS Layer/sqsClient.ts
@@ -1,5 +1,6 @@
 import * as dotenv from "dotenv"
 import { SQSClient, CreateQueueCommand, GetQueueUrlCommand, GetQueueAttributesCommand, SetQueueAttributesCommand } from "@aws-sdk/client-sqs"
+import { stringify } from "querystring"
 
 dotenv.config({ path: "/.env"})
 
@@ -7,6 +8,9 @@ dotenv.config({ path: "/.env"})
 const region = process.env["REGION"] || "REGION NOT DEFINED IN .ENV"
 const accessKeyId = process.env["AWS_ACCESS_KEY_ID"] || "AWS ACCESS KEY NOT DEFINED IN .ENV"
 const secretAccessKey = process.env["AWS_SECRET_ACCESS_KEY"] || "AWS SECRET ACCESS KEY REGION NOT DEFINED IN .ENV"
+
+// uninitialized placeholder for name of queue to use for sqs operations ( see first line of try block in 'createQueue' function where it is initialized before the creation of a queue for the task )
+var sqsQueueName = ""
 
 // Create SQS client to send commands to
 const attributes = {
@@ -18,13 +22,14 @@ const attributes = {
 }
 const client = new SQSClient(attributes)
 
-async function createQueue(queueName:string){
+async function createQueue(clientToUse: SQSClient | any=client){
     try{
+        sqsQueueName = "AmazonRekognitionQueue" + String(Date.now())
         const attributes = {
-            QueueName: queueName
+            QueueName: sqsQueueName
         }
         const command = new CreateQueueCommand(attributes)
-        const result = await client.send(command)
+        const result = await clientToUse.send(command)
         return result || {error:"Error"}
     }catch(e){
         console.log(e)
@@ -32,13 +37,13 @@ async function createQueue(queueName:string){
     }
 }
 
-async function getQueueUrl(queueName:string){
+async function getQueueUrl(queueName: string, clientToUse: SQSClient | any=client){
     try{
         const attributes = {
             QueueName: queueName
         }
         const command = new GetQueueUrlCommand(attributes)
-        const result = await client.send(command)
+        const result = await clientToUse.send(command)
         return result || {error:"Error"}
     }catch(e){
         console.log(e)
