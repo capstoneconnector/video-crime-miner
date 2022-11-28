b886ed431bdf214c5a7e5576db20c017bfdc0008
Jacob Bishop <jacobbishop1000@gmail.com>
2022-11-28 17:37:15 -0500

    Thinking we use POST requests to the deployed node backend but with that I'm not sure exactly where the connector belongs

--- node-video-crime-miner/src/Node Connector Layer/rekognitionConnector.ts
+++ node-video-crime-miner/src/Node Connector Layer/rekognitionConnector.ts
@@ -2,9 +2,29 @@
 
 // TODO: Figure out how to connect this to the frontend
 
-function UploadVideo(form:any){
+async function UploadVideo(form:any){
     // TODO: Allow user to upload Video to AWS
-    return null
+    //return null
+    var url = ""
+    var content = ""
+
+    const response = await fetch(url, {
+        method: 'POST',
+        body: content,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'} })
+      
+    if (!response.ok) {
+        /* Handle */ 
+    }
+    
+    // If you care about a response:
+    if (response.body !== null) {
+        // body is ReadableStream<Uint8Array>
+        // parse as needed, e.g. reading directly, or
+        const asString = ""//new TextDecoder("utf-8").decode(response.body);
+        // and further:
+        const asJSON = JSON.parse(asString);  // implicitly 'any', make sure to verify type on runtime.
+    }
 }
 
 export { UploadVideo }
