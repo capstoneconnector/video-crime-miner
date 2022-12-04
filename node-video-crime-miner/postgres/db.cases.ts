451f2bd63b7d89956b46d38e3769eee902ee61a9
Jacob Bishop <jacobbishop1000@gmail.com>
2022-12-04 01:15:28 -0500

    Removed debugging comments

--- node-video-crime-miner/postgres/db.cases.ts
+++ node-video-crime-miner/postgres/db.cases.ts
@@ -1,12 +1,7 @@
 import {pool} from './db.config.js'
 
 async function getCases() {
-    console.log("in getcases()")
     try {
-        //await pool.connect()
-      // Querying the client returns a query result promise
-      // which is also an asynchronous result iterator.
-      console.log("getting query")
       const query = await pool.query(
           "SELECT * FROM public.case"
       )
@@ -15,12 +10,5 @@ async function getCases() {
       console.log({error:e})
       return e
     }
-    /*
-    finally {
-      await pool.end()
-      console.log("exited postgres")
-    }
-    */
   }
-  
   export {getCases}
