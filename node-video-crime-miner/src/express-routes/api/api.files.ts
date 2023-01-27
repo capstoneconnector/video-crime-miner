f884845491e7ec938ccca240639afd4689dafcf5
Merghani Hag Elnour <Mthagelnour@bsu.edu>
2023-01-26 19:10:59 -0500

    fix bug where the files/download always errored out

--- node-video-crime-miner/src/express-routes/api/api.files.ts
+++ node-video-crime-miner/src/express-routes/api/api.files.ts
@@ -9,7 +9,7 @@ import { Readable } from 'stream'
 /* GET all files in S3 Bucket */
 async function fetchAllFiles(req: Request, res: Response, next: NextFunction) {
     try {
-      const files = await listObjects("video-crime-miner-video-test-bucket")
+      const files = await listObjects("mt-vcm-uploads")
       return res.status(200).json(files)
     } catch (err) {
       console.log("app.get('/files') errored out")
@@ -20,10 +20,11 @@ async function fetchAllFiles(req: Request, res: Response, next: NextFunction) {
 /* GET file in S3 Bucket by File Name */
 async function fetchFileByName(req: any, res: Response, next: NextFunction) {
     try {
-		var result = await getObjectFromS3("video-crime-miner-video-test-bucket" , req.params.file[0])
-		if(result instanceof Readable){
-      result.pipe(res)
-    }
+		var result = await getObjectFromS3("mt-vcm-uploads" , req.params.file)
+
+		if(result instanceof Readable) {
+      	result.pipe(res)
+	}
 		return res.status(200)
 	} catch (err) {
 		res.status(500).send(err)
@@ -34,7 +35,7 @@ async function fetchFileByName(req: any, res: Response, next: NextFunction) {
 async function createAndUploadFile(req: any, res: Response, next: NextFunction) {
     try {
         const saveFile = ""
-        const result= await upload("video-crime-miner-video-test-bucket", req.body.file)
+        const result= await upload("mt-vcm-uploads", req.body.file)
         const dbresult = await createNewFileRow(req.body.file.name, "", 4)
         console.log({s3: result, db: dbresult})
         return res.status(200).json({
