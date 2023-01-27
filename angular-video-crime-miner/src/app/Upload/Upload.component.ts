61c40f08a13489304159fc759b76555eb5e06998
Merghani Hag Elnour <Mthagelnour@bsu.edu>
2023-01-27 02:07:36 -0500

    update upload component

--- angular-video-crime-miner/src/app/Upload/Upload.component.ts
+++ angular-video-crime-miner/src/app/Upload/Upload.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpEventType, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { FileService } from 'src/app/file.service';
 import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
 
 @Component({
@@ -11,21 +10,19 @@ import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
 })
 export class UploadComponent {
 
-  selectedFiles?: FileList;
-  currentFile?: File;
-  progress = 0;
-  message = '';
-
   fileInfos?: Observable<any>;
   private baseUrl = 'http://localhost:8000';
 
-
-
-  constructor(private uploadService: FileService, private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.fileInfos = this.uploadService.getFiles()
+
   }
+
+  // On file Select
+  onChange(event:any) {
+	this.file = event.target.files[0];
+}
 
   submitCase(data:any): void{
     let headers = new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'})
@@ -38,44 +35,26 @@ export class UploadComponent {
     req.subscribe()
   }
 
-  selectFile(event: any): void {
-    this.selectedFiles = event.target.files;
+  name: string = ''
+  file: any
+
+  getName(name: string) {
+	this.name = name
+  }
+
+  getFile(event:any) {
+	this.file = event.target.files[0]
   }
 
   upload(): void {
-    this.progress = 0;
 
-    if (this.selectedFiles) {
-      const file: File | null = this.selectedFiles.item(0);
+	let formData = new FormData();
+	formData.set("name", this.name)
+	formData.set("file", this.file)
 
-      if (file) {
-        this.currentFile = file;
-
-        this.uploadService.upload(this.currentFile).subscribe({
-          next: (event: any) => {
-            if (event.type === HttpEventType.UploadProgress) {
-              this.progress = Math.round(100 * event.loaded / event.total);
-            } else if (event instanceof HttpResponse) {
-              this.message = event.body.message;
-              this.fileInfos = this.uploadService.getFiles();
-            }
-          },
-          error: (err: any) => {
-            console.log(err);
-            this.progress = 0;
-
-            if (err.error && err.error.message) {
-              this.message = err.error.message;
-            } else {
-              this.message = 'Could not upload the file!';
-            }
-
-            this.currentFile = undefined;
-          }
-        });
-      }
-
-      this.selectedFiles = undefined;
-    }
+	this.http
+	.post(this.baseUrl + "/upload", formData)
+	.subscribe((response:any) => {})
+    
   }
 }
