d9af04c7abcef32cdd4d2ec67a851915386cfc21
ejperezz <ejperez@bsu.edu>
2023-01-27 13:53:35 -0500

    updated deleting files

--- angular-video-crime-miner/src/app/app-routing.module.ts
+++ angular-video-crime-miner/src/app/app-routing.module.ts
@@ -5,19 +5,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MediaComponent } from './media/media.component';
 import { ReportsComponent } from './reports/reports.component';
 import { UploadComponent } from './upload/Upload.component';
-import { SettingsComponent } from './settings/settings.component';
-import { StatsComponent } from './stats/stats.component';
 import { FileRekognitionViewComponent } from './file-rekognition-view/file-rekognition-view.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'upload', component: UploadComponent},
-  {path: 'stats', component: StatsComponent},
   {path: 'view', component: ViewComponent},
   {path: 'reports', component: ReportsComponent},
   {path: 'media', component: MediaComponent},
-  {path: 'settings', component: SettingsComponent},
   {path: 'file-rekognition-view/:jobId', component: FileRekognitionViewComponent}
 ];
 
