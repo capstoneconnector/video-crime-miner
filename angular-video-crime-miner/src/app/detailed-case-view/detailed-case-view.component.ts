f32b01cd24207c75122c4606fde15725d65c55c4
Jacob Bishop <jacobbishop1000@gmail.com>
2023-01-30 23:05:01 -0500

    detailed case case info section now working

--- angular-video-crime-miner/src/app/detailed-case-view/detailed-case-view.component.ts
+++ angular-video-crime-miner/src/app/detailed-case-view/detailed-case-view.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-detailed-case-view',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailedCaseViewComponent implements OnInit {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8000'
+  private caseId!: string
+  private caseInfo?: JSON
+  private caseFiles?: JSON
+  private caseOutputs?: JSON
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.caseId = this.route.snapshot.paramMap.get('caseId') || '1'
+    this.requestCaseInfo().subscribe(res => {
+      this.caseInfo = res
+    })
+    this.requestCaseFiles().subscribe(res => {
+      this.caseFiles = res
+    })
+    //this.requestCaseOutputs().subscribe(res => {
+    //  this.outputs = res
+    //})
   }
 
+  public requestCaseInfo(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/cases/${this.caseId}`)
+  }
+
+  public getCaseInfo(): any {
+    return this.caseInfo
+  }
+
+  public requestCaseFiles(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/files/case/${this.caseId}`)
+  }
+
+  public getCaseFiles(): any {
+    return this.caseFiles
+  }
+
+/*
+  public requestCaseOutputs(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/labels/case/${this.caseId}`)
+  }
+*/
+  public getCaseOutputs(): any {
+    return this.caseOutputs
+  }
 }
