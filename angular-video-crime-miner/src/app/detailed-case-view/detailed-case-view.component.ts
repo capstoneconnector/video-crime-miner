14b49ceff61df9fd1ed52bce715f027419a2af24
Jacob Bishop <jacobbishop1000@gmail.com>
2023-01-30 21:01:23 -0500

    Added detailed-case-view component Fixed scrolling on file rekognition view

---
+++ angular-video-crime-miner/src/app/detailed-case-view/detailed-case-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detailed-case-view',
+  templateUrl: './detailed-case-view.component.html',
+  styleUrls: ['./detailed-case-view.component.scss']
+})
+export class DetailedCaseViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
