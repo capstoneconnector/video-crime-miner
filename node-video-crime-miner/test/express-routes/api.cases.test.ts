e6290e1bb157746a2e5393c285b97164ebe55c2a
Jacob Bishop <jacobbishop1000@gmail.com>
2023-01-31 16:42:02 -0500

    Got boiler plate stuff for api jest tests in

---
+++ node-video-crime-miner/test/express-routes/api.cases.test.ts
@@ -0,0 +1,9 @@
+
+
+describe("fetchAllCases function", () => {
+
+	it("fetchAllCases() should return a valid JSON response", () => {
+		const res = {exampleJSONkey: "exampleJSONvalue"}
+		expect(res).toBeTruthy()
+	})
+})
---
+++ node-video-crime-miner/test/express-routes/api.files.test.ts
@@ -0,0 +1,9 @@
+
+
+describe("fetchAllFiles function", () => {
+
+	it("fetchAllFiles() should return a valid JSON response", () => {
+		const res = {exampleJSONkey: "exampleJSONvalue"}
+		expect(res).toBeTruthy()
+	})
+})
