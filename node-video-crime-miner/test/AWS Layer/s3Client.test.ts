5a977d4e8d8363afd700ee4eb46fbe631be92cbe
Jacob Bishop <jacobbishop1000@gmail.com>
2023-02-03 14:47:09 -0500

    Got test coverage to almost 80%

--- node-video-crime-miner/test/AWS Layer/s3Client.test.ts
+++ node-video-crime-miner/test/AWS Layer/s3Client.test.ts
@@ -1,64 +1,74 @@
-
-
 import { mockClient } from "aws-sdk-client-mock"
 import { S3Client, CreateBucketCommand, ListBucketsCommand, ListObjectsV2Command, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3"
 import { createBucket, listBuckets, listObjects, upload, getObjectFromS3, uploadWithFile } from "../../src/AWS Layer/s3Connector"
 
 var s3Mock = mockClient(S3Client)
 
+//createBucket function test
 describe("createBucket function", () => {
 	s3Mock.on(CreateBucketCommand).resolves({})
 	it("Should return a response after sending a request to create bucket", async () => {
 		const res = await createBucket("example bucket name")
-		//const res = createBucketMock("Example Bucket")
 		expect(res).toBeTruthy()
 	})
 })
 
 //TODO: Write out these functions just like the first one above is written!
-/*
-describe('listObjects function', () => {
 
-	it('Should return a response in dictionary format', async() => {
-		const res = 1
+//listBucket function test
+describe('listBuckets function', () => {
+	s3Mock.on(ListBucketsCommand).resolves({})
+	it('Should return a list of buckets', async() => {
+		const res = await listBuckets()
 		expect(res).toBeTruthy()
 	})
 })
 
-
-
-describe("listBucket function", () => {
-
-	it("Should return a list of buckets", async() => {
-		const res = 1
+//listObjects function test
+describe("listObjects function", () => {
+	s3Mock.on(ListObjectsV2Command).resolves({})
+	it("Should return a list of objects", async() => {
+		const res = await listObjects("example bucket")
 		expect(res).toBeTruthy()
 	})
 })
 
+//upload function test
 describe("upload function", () => {
-
-	it("Should return successfull response after uploading image file", async () => {
-		const res = 1
+	s3Mock.on(PutObjectCommand).resolves({})
+	it("Should return successful response after uploading image file", async () => {
+		const res = await upload("example bucket", "filename.img")
 		expect(res).toBeTruthy()
 	})
 
-	it("Should return successfull response after uploading video file", async () => {
-		const res = 1
+	it("Should return successful response after uploading video file", async () => {
+		const res = await upload("example bucket", "filename.mp4")
+		expect(res).toBeTruthy()
+	})
+	// TODO: Test for failure with non-image and non-video files
+	/*
+	it("Should return failure after uploading any non-image and non-video files", async () => {
+		const res = await upload("example bucket", "filename.pdf")
+		expect(res).toBeFalsy()
+	})
+	*/
+})
+
+//getObjectFromS3 function test
+describe("getObjectFromS3 function", () => {
+	s3Mock.on(GetObjectCommand).resolves({})
+	it("Should return successful response", async () => {
+		const res = await getObjectFromS3("example bucket", "filename.png")
 		expect(res).toBeTruthy()
 	})
 })
 
-describe("download function", () => {
-
-	it("Should return successfull response after uploading image file", async () => {
-		const res = 1
-		expect(res).toBeTruthy()
-	})
-
-	it("Should return successfull response after uploading video file", async () => {
-		const res = 1
+//uploadWithFile function test
+describe("uploadWithFile function", () => {
+	const uint = new Uint8Array()
+	s3Mock.on(PutObjectCommand).resolves({})
+	it("Should return successful response", async () => {
+		const res = await uploadWithFile("example bucket", uint, {})
 		expect(res).toBeTruthy()
 	})
 })
-
-*/
