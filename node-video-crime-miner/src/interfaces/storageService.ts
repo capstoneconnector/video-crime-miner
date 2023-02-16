246f66f403098bc07005adae5d957458b8c7f39e
Merghani Hag Elnour <Mthagelnour@bsu.edu>
2023-02-15 21:51:52 -0500

    Decoupling improvements

--- node-video-crime-miner/src/interfaces/storageService.ts
+++ node-video-crime-miner/src/interfaces/storageService.ts
@@ -1,7 +1,6 @@
 
 /* Backend S3 Storage Service imports */
 import { listObjects, getObjects, uploadFrontEndClient } from '../AWS Layer/s3Connector.js'
-
 interface storageService {
 	listObjects(source: string /*parameter type*/): any /*return type*/;
 
@@ -12,7 +11,7 @@ interface storageService {
 	/* ADD more functions a storage serivce has that we may need here*/
 }
 
-const s3: storageService = {
+const s3 : storageService = {
 
 	// I only imported the functions we need for the angular client for now
 	listObjects: function (bucket: string): any {
@@ -31,11 +30,11 @@ const s3: storageService = {
 }
 
 /* FOR EXAMPLE PURPOSES:
-Using interfaces we can add other cloud storage 
+Using interfaces we can implement add other cloud storage 
 services like Google Cloud Storage or Azure easily.
 */
 
-var anotherStorageService : storageService = {
+const azure : storageService = {
 	listObjects: function (source: string) {
 		throw new Error('Function not implemented.');
 	},
@@ -47,7 +46,29 @@ var anotherStorageService : storageService = {
 	}
 }
 
-export {s3}
+function getStorageService() {
+
+	/*This function compares STORAGE_SERVICE variable from .env and returns the implementation(service) */
+
+	let service = process.env['STORAGE_SERVICE'] || 'STORAGE SERVICE NOT DEFINED IN .env'
+
+	if (service == 's3' || 'S3' || 'aws_s3' || 'AWS_S3') {
+		return s3
+
+	} else if (service == 'azure' || 'Azure') {
+		return azure
+
+	}
+
+	console.log("The Cloud Service in use is : " + service)
+}
+
+/*Get Storage Service variable from env*/
+let envVariable = getStorageService()
+
+
+
+export {envVariable}
 
 
 
