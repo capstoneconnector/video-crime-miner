391a3fda56a393352287a455b97c1e438973d62c
Jacob Bishop <jacobbishop1000@gmail.com>
2023-03-18 17:45:19 -0400

    added cognito pool id and web client id

--- angular-video-crime-miner/src/environment/environment.prod.ts
+++ angular-video-crime-miner/src/environment/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   cognito: {
-    userPoolId: '',
-    userPoolWebClientId: '',
+    userPoolId: 'us-east-1_D7l3dpYJi',
+    userPoolWebClientId: '5nj0s5iom3bosajkpcrk4u9b6b'
   }
 }
 
--- angular-video-crime-miner/src/environment/environment.ts
+++ angular-video-crime-miner/src/environment/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: false,
   cognito: {
-    userPoolId: '',
-    userPoolWebClientId: '',
+    userPoolId: 'us-east-1_D7l3dpYJi',
+    userPoolWebClientId: '5nj0s5iom3bosajkpcrk4u9b6b'
   }
 }
