9ad878b787ee12ddae2c46b93079d3d189e66354
Jacob Bishop <jacobbishop1000@gmail.com>
2023-03-18 17:56:35 -0400

    removed temporary cognito environment values

--- angular-video-crime-miner/src/environment/environment.ts
+++ angular-video-crime-miner/src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   cognito: {
-    userPoolId: 'us-east-1_D7l3dpYJi',
-    userPoolWebClientId: '5nj0s5iom3bosajkpcrk4u9b6b'
+    userPoolId: '',
+    userPoolWebClientId: '',
   }
 }
