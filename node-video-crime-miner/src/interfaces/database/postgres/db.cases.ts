bdea6ccc69d28f1621dc6e25e4293ac6e9ee712f
Illuminubby <illuminubby@gmail.com>
2023-03-19 02:23:12 -0400

    adapt functions interacting with case table to utilize new user_id column

--- node-video-crime-miner/src/interfaces/database/postgres/db.cases.ts
+++ node-video-crime-miner/src/interfaces/database/postgres/db.cases.ts
@@ -3,10 +3,11 @@ import { pool } from './db.config.js'
 /* Domain Model Imports */
 import ChuqlabCase from '../../../model/ChuqlabCase.js'
 
-async function getAllCases(): Promise<ChuqlabCase[]> {
+async function getAllCases(username:string): Promise<ChuqlabCase[]> {
   try {
     const query = await pool.query(
-      'SELECT * FROM public.case'
+      'SELECT * FROM public.case WHERE user_id=($1)',
+      [username]
     )
     const rows = query.rows
     var result = new Array<ChuqlabCase>
@@ -20,11 +21,11 @@ async function getAllCases(): Promise<ChuqlabCase[]> {
   }
 }
 
-async function getCaseById (id: string) {
+async function getCaseById (id: string, username:string) {
   try {
     const query = await pool.query(
-      'SELECT * FROM public.case WHERE case_id=($1)',
-      [id]
+      'SELECT * FROM public.case WHERE case_id=($1) AND user_id=($2)',
+      [id, username]
     )
     const rows = query.rows
     var result = new Array<ChuqlabCase>
@@ -38,11 +39,11 @@ async function getCaseById (id: string) {
   }
 }
 
-async function insertNewCase (name: string, description: string, tags: string[]) {
+async function insertNewCase (name: string, description: string, tags: string[], username:string) {
   try {
     const query = await pool.query(
-      'INSERT INTO public.case (name, description, tags) VALUES ($1, $2, $3)',
-      [name, description, tags]
+      'INSERT INTO public.case (name, description, tags, user_id) VALUES ($1, $2, $3, $4)',
+      [name, description, tags, username]
     )
     return
   } catch (e) {
