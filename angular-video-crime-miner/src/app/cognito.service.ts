d3514e4bac074e13d9f1b123af815e3ba40b5427
Jacob Bishop <jacobbishop1000@gmail.com>
2023-03-19 18:47:56 -0400

    view component ready for user update from backend

--- angular-video-crime-miner/src/app/cognito.service.ts
+++ angular-video-crime-miner/src/app/cognito.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Auth, Amplify } from 'aws-amplify';
-import { CognitoUser, CognitoUserPool } from 'amazon-cognito-identity-js';
+import { Injectable } from '@angular/core'
+import { BehaviorSubject } from 'rxjs'
+import { Auth, Amplify } from 'aws-amplify'
+import { CognitoUser, CognitoUserPool } from 'amazon-cognito-identity-js'
 
-import { environment } from '../environment/environment';
+import { environment } from '../environment/environment'
 
 export interface IUser {
-  email: string;
-  password: string;
-  showPassword: boolean;
-  code: string;
-  name: string;
+  email: string
+  password: string
+  showPassword: boolean
+  code: string
+  name: string
 }
 
 @Injectable({
@@ -21,74 +21,82 @@ export class CognitoService {
   private authenticationSubject: BehaviorSubject<boolean>
   private cognitoUserPool: CognitoUserPool
   private cognitoUser: CognitoUser | null
-
-  public getAuthenticationSubject(): BehaviorSubject<boolean> {
-    return this.authenticationSubject
-  }
+  private username: BehaviorSubject<string>
 
   constructor() {
     Amplify.configure({
       Auth: environment.cognito,
-    });
+    })
 
     this.authenticationSubject = new BehaviorSubject<boolean>(false)
+    this.username = new BehaviorSubject<string>("")
 
     const poolData = {
       UserPoolId: environment.cognito.userPoolId,
       ClientId: environment.cognito.userPoolWebClientId
     }
 
-    this.cognitoUserPool = new CognitoUserPool(poolData);
+    this.cognitoUserPool = new CognitoUserPool(poolData)
     this.cognitoUser = this.cognitoUserPool.getCurrentUser()
     if (this.cognitoUser) {
-      this.authenticationSubject.next(true);
+      this.authenticationSubject.next(true)
+      this.username.next(this.cognitoUser.getUsername())
     }
+  }
+
+  public getUsername(): BehaviorSubject<string> {
+    return this.username
+  }
+
+  public getAuthenticationSubject(): BehaviorSubject<boolean> {
+    return this.authenticationSubject
   }
 
   public signUp(user: IUser): Promise<any> {
     return Auth.signUp({
       username: user.email,
       password: user.password,
-    });
+    })
   }
 
   public confirmSignUp(user: IUser): Promise<any> {
-    return Auth.confirmSignUp(user.email, user.code);
+    return Auth.confirmSignUp(user.email, user.code)
   }
 
   public async signIn(user: IUser): Promise<any> {
-    await Auth.signIn(user.email, user.password);
-    this.authenticationSubject.next(true);
+    await Auth.signIn(user.email, user.password)
+    this.authenticationSubject.next(true)
   }
 
   public async signOut(): Promise<any> {
-    await Auth.signOut();
-    this.authenticationSubject.next(false);
+    await Auth.signOut()
+    this.authenticationSubject.next(false)
+    this.username.next("")
   }
 
   public async checkIfAuthenticated(): Promise<boolean> {
     if (this.authenticationSubject.value) {
-      return Promise.resolve(true);
+      return Promise.resolve(true)
     } else {
       return this.getUser()
       .then((user: any) => {
         if (user) {
-          return true;
+          return true
         } else {
-          return false;
+          return false
         }
       }).catch(() => {
-        return false;
-      });
+        return false
+      })
     }
   }
 
-  public getUser(): Promise<any> {
-    return Auth.currentUserInfo();
+  public async getUser(): Promise<any> {
+    return Auth.currentUserInfo()
   }
 
   public async updateUser(user: IUser): Promise<any> {
-    const cognitoUser = await Auth.currentUserPoolUser();
-    return await Auth.updateUserAttributes(cognitoUser, user);
+    const cognitoUser = await Auth.currentUserPoolUser()
+    return await Auth.updateUserAttributes(cognitoUser, user)
   }
 }
