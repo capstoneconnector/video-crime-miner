50147dde828f40f315944dfb72de774453a186eb
Illuminubby <illuminubby@gmail.com>
2023-04-03 13:23:30 -0400

    add webhook functions getJobStatusByTags, used to check status of batch jobs, and fetchDistinctKeywords, used to find distinct sets of keywords for given case

--- node-video-crime-miner/src/express-routes/controllers/api.labels.ts
+++ node-video-crime-miner/src/express-routes/controllers/api.labels.ts
@@ -215,15 +215,154 @@ async function getJobStatusByJobID (req: Request, res: Response, next: NextFunct
 
 }
 
+/* GET label detection job status of multiple jobs based on tags */
+async function getJobStatusByTags (req: Request, res: Response, next: NextFunction) {
+
+  try {
+
+    var response = emptyOutput
+
+    // user-defined distinct tags for batch job
+    const keywords = req.params['tags'].split(',')
+
+    // fetches relevant filenames from awsoutput Table by distinct tags
+    const jobsInfo = await databaseService.fetchFilesByKeywords(keywords)
+
+    // placeholder for job Ids to be extracted from jobsInfo
+    var jobIdsToCheck:string[] = []
+
+    jobsInfo.forEach( (job:any) => {
+      jobIdsToCheck.push(job.job_id)
+    })
+
+    // initially true, sets to false if any relevant video analysis jobs aren't completed
+    var statusCheck = true
+
+    //console.log("Checking Status of JobIds: ", jobIdsToCheck)
+
+    // For each job ID, check completion status 
+    for (let i = 0; i < jobIdsToCheck.length; i++) {  
+
+      // fetches QueueURL and TopicArn for SQS and SNS services
+      var statParams = await databaseService.fetchQUrlByJobId( jobIdsToCheck[i] )
+
+      // checks db flag for whether job has ever been confirmed to have finished
+      if (String(statParams.QueueUrl).length > 2) {
+
+        // db flag for finished job was not present, so check SQS for status
+        const stat = await vidService.checkJobStatus( statParams.QueueUrl )
+
+        //console.log("stat var: ", stat)
+
+        //checks for message content in SQS for SNS topic
+        if (JSON.stringify( stat ).includes('Body')) {
+          // job finished
+          
+          for (var message of stat.Messages) {
+            var notification = JSON.parse(message.Body)
+            var rekMessage = JSON.parse(notification.Message)
+            var messageJobId = rekMessage.JobId
+  
+            // checks for message relevant to job
+            if ( String( messageJobId ).includes( jobIdsToCheck[i] ) ) {
+  
+              // checks if job was completed successfully, delete SQS and SNS instances, raise db flag for job finished
+              if ( String( rekMessage.Status ).includes( String("SUCCEEDED") ) ) {
+                //console.log("rekMessage.Status of " + keywords.toString() + ": ", rekMessage.Status)
+                await vidService.clearNotifications(statParams.QueueUrl, statParams.TopicArn)
+                await databaseService.markJobAsFinished( jobIdsToCheck[i] )
+              }
+              else {
+                // batch job is not finished
+                statusCheck = false
+              }
+  
+            }
+  
+          }
+  
+        }
+        else {
+          // batch job is not finished
+          statusCheck = false
+        }
+
+      }
+      
+      
+
+    }
+    
+    //console.log("Status of " + keywords.toString() + ": " + statusCheck)
+
+    // if all jobs in batch job are finished
+    if (statusCheck) {
+
+      // loop through all jobs in batch job
+      for (let i = 0; i < jobIdsToCheck.length; i++) {
+
+        // checks db for job results
+        var labelsRetrievedForJob = await databaseService.getResultsForJob(jobIdsToCheck[i])
+
+        //if (response.data != null) { console.log( "Fetching results from db: ", jobIdsToCheck[i] ) }
+
+        // if the result is null, it's not stored in the db yet. Let's see what AWS has to say about it!
+        if (labelsRetrievedForJob == null) {
+          //console.log( "Fetching results for first time: ", jobIdsToCheck[i] )
+
+          // fetch job results from Video Analysis Service
+          let newResult = await vidService.collectJobResults(jobIdsToCheck[i]) // Get results for the id
+
+          // Now let's trim down the result to only include labels and video metadata
+          let newData = {
+            Labels: newResult[0],
+            VideoMetadata: newResult[1]
+          }
+          labelsRetrievedForJob = {
+            Labels: newResult[0],
+            VideoMetadata: newResult[1]
+          }
+          await databaseService.updateJobResults(jobIdsToCheck[i], newData) // update the db entry
+          // JobStatus for the AWS Rekognition return is an element of the following set: {IN_PROGRESS, SUCCEEDED, FAILED}
+        }
+
+      }
+      // notifies front-end that the batch job is completed and viewable on file-rekognition-view
+      response.data = "finished"
+    }
+    else {
+      //notifies front-end that batch job is still running
+      response.data = "pending"
+    }
+    //console.log("Status of (" + keywords.toString() + "): ", response.data)
+    response.success = true
+    response = standardizeResponse(response).convertToJson()
+    res.status(200).json(response)
+
+  } catch (err: any) {
+    console.log("app.get('/labels/tags/status/') errored out")
+    response.errors.push(err.message)
+    response.success = false
+    response = standardizeResponse(response).convertToJson()
+    res.status(500).json(response)
+  }
+
+}
+
 /* GET AWS Labels Results for all jobs on a case using given keywords */
 async function fetchFilesByKeywords (req: Request, res: Response, next: NextFunction) {
   try {
     
     var response = emptyOutput
 
-    const keywords = req.body.labels || []
+    const keywords = req.params['keywords'].split(',')
+
+    //console.log( "PARAM Keywords (api.labels): ", req.params['keywords'] )
+    //console.log( "Keywords (api.labels): ", keywords )
 
     response.data = await databaseService.fetchFilesByKeywords(keywords)
+
+    //console.log( "FilesByKeywords: ", response.data )
 
     response.success = true
     response = standardizeResponse(response).convertToJson()
@@ -237,5 +376,32 @@ async function fetchFilesByKeywords (req: Request, res: Response, next: NextFunc
   }
 }
 
+/* GET All distinct tags for jobs run on given case */
+async function fetchDistinctKeywords (req: Request, res: Response, next: NextFunction) {
 
-export { fetchLabelDetectionJob, fetchLabelDetectionIdsForFile, fetchAllLabelDetectionForMultipleFiles, createNewLabelDetectionJob, fetchFileForJobID, createMultiLabelJob, fetchFilesByKeywords, getJobStatusByJobID }
+  try{
+
+    var response = emptyOutput
+
+    const caseId = req.params['caseId']
+
+    //console.log("CaseId: ", caseId)
+
+    response.data = await databaseService.fetchDistinctKeywords(caseId)
+
+
+    response.success = true
+    response = standardizeResponse(response).convertToJson()
+    res.status(200).json(response)
+
+  } catch (err:any) {
+    console.log("app.get('/case/keywords') errored out")
+    response.errors.push(err.message)
+    response.success = false
+    response = standardizeResponse(response).convertToJson()
+    res.status(500).json(response)
+  }
+
+}
+
+export { fetchLabelDetectionJob, fetchLabelDetectionIdsForFile, fetchAllLabelDetectionForMultipleFiles, createNewLabelDetectionJob, fetchFileForJobID, createMultiLabelJob, fetchFilesByKeywords, getJobStatusByJobID, fetchDistinctKeywords, getJobStatusByTags }
