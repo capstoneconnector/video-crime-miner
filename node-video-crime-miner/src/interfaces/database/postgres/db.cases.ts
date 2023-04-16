bebb24568c0948cca1c28efde2991dd7f2227714
Merghani Hag Elnour <Mthagelnour@bsu.edu>
2023-04-16 19:50:41 -0400

    Implement the functionality

--- node-video-crime-miner/src/interfaces/database/postgres/db.cases.ts
+++ node-video-crime-miner/src/interfaces/database/postgres/db.cases.ts
@@ -54,17 +54,44 @@ async function insertNewCase (name: string, description: string, tags: string[],
 
 async function updateCaseDetails (case_id:string, name:string, description: string, tags:string[], notes:string, username:string) {
 	try {
-		const query = await pool.query('UPDATE public.case SET description = $1, tags = $2, notes = $3, name = $4 WHERE case_id = $5 AND WHERE user_id = $6',
+	  const query = await pool.query('UPDATE public.case SET description = $1, tags = $2, notes = $3, name = $4 WHERE case_id = $5 AND user_id = $6',
 		[description, tags, notes, name, case_id, username])
-		console.log(`Case ${case_id} updated successfully.`)
-
-		return `Case ${case_id} updated successfully. Name: Renamed to ${name}. Description: Renamed to ${description}, Tags: Renamed to ${tags}, Notes: Renamed to ${notes}`
-
+	  console.log(`Case ${case_id} updated successfully.`)
+  
+	  return `Case ${case_id} updated successfully. Name: Renamed to ${name}. Description: Renamed to ${description}, Tags: Renamed to ${tags}, Notes: Renamed to ${notes}`
+  
 	} catch (e) {
-		console.log({error:e})
-		return e
+	  console.log({error:e})
+	  return e
 	}
+  }
 
-}
+  async function deleteCase(case_id: string) {
+	try {
+	  // Delete associated awsoutput files
+	  const query1 = await pool.query(
+		'DELETE FROM public.awsoutput WHERE file_id IN (SELECT s3_name FROM public.file WHERE case_id = $1)',
+		[case_id]
+	  );
+  
+	  // Delete associated files
+	  const query2 = await pool.query(
+		'DELETE FROM public.file WHERE case_id = $1',
+		[case_id]
+	  );
+  
+	  // Delete case
+	  const query3 = await pool.query(
+		'DELETE FROM public."case" WHERE case_id = $1',
+		[case_id]
+	  );
+  
+	  console.log(`Case ${case_id} deleted successfully`);
+	} catch (e) {
+	  console.log({ error: e });
+	  return e;
+	}
+  }
 
-export { getAllCases, insertNewCase, getCaseById, updateCaseDetails }
+
+export { getAllCases, insertNewCase, getCaseById, updateCaseDetails, deleteCase }
